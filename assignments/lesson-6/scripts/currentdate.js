27ab2c695a238db001697a814e75edbd0e69d340
js-goff <38774971+js-goff@users.noreply.github.com>
2018-06-02 11:30:30 -0600

    lesson-6 upload
    not many changes still working in progress.

---
+++ assignments/lesson-6/scripts/currentdate.js
@@ -0,0 +1,10 @@
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var now = new Date();
+var month = now.getMonth();
+var dayOfMonth = now.getDate();
+var dayOfWeek = now.getDay();
+var dayOfWeekName = weekday[dayOfWeek];
+var monthName = months[month];
+var fullYear = now.getFullYear();
+document.getElementById("currentdate").innerHTML = dayOfWeekName + ", " + dayOfMonth + " " + monthName + " " + fullYear;
