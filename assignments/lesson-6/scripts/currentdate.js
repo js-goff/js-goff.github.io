117d7de2e0468be15b188f3d1f30cfa76da5c53f
js-goff <38774971+js-goff@users.noreply.github.com>
2018-06-04 21:33:50 -0600

    Franklin pages
    Franklin Page

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,10 +1,32 @@
-var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var now = new Date();
-var month = now.getMonth();
-var dayOfMonth = now.getDate();
-var dayOfWeek = now.getDay();
-var dayOfWeekName = weekday[dayOfWeek];
-var monthName = months[month];
-var fullYear = now.getFullYear();
-document.getElementById("currentdate").innerHTML = dayOfWeekName + ", " + dayOfMonth + " " + monthName + " " + fullYear;
+let d = new Date();
+    let weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var dayOfWeek = weekday[d.getDay()];
+	
+let month = new Array(12);
+	month[0] = "January";
+	month[1] = "February";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "August";
+	month[8] = "September";
+	month[9] = "October";
+	month[10] = "November";
+	month[11] = "December";
+	let monthName = month[d.getMonth()];
+
+let day = d.getDate();
+
+let year = d.getFullYear();
+
+let combinedDate = dayOfWeek + ", " + day + " " + monthName + " " + year;
+document.getElementById("currentdate").innerHTML = combinedDate;
