b1cc3c9d95be554ab8983917ed13a614f20efc5d
js-goff <38774971+js-goff@users.noreply.github.com>
2018-06-12 11:09:37 -0600

    windchill working
    windchill update

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -3,5 +3,5 @@ let l= parseFloat(document.getElementById("low").innerHTML);
 let w= parseFloat(document.getElementById("wind").innerHTML);
 let avetemp=(h+l)/2;
 let se= Math.pow(w, 0.16);
-let f= 35.74 + 0.6215(avetemp)-35.75(se)+ 0.4275(avetemp)(se);
-document.getElementById("windchill").innerHTML = f;
+let f= 35.74 + 0.6215*(avetemp)-35.75*(se)+ 0.4275*(avetemp)*(se);
+document.getElementById("windchill").innerHTML =Math.round(f);
