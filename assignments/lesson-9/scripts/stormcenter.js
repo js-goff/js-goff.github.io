c049242e74c646097d3f07d724472eef3be6fde6
js-goff <38774971+js-goff@users.noreply.github.com>
2018-06-25 16:46:16 -0600

    Modifications to weathersite
    Just some changes

---
+++ assignments/lesson-9/scripts/stormcenter.js
@@ -0,0 +1,8 @@
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function openInNewTab(url) {
+  var win = window.open(url, '_thank you.html');
+  win.focus();
+}
---
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -0,0 +1,12 @@
+//connection to my API
+let weatherRequest = new XMLHttpRequest();
+let apiURL='https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=4c5ecbb4699bafc4a2d884937c4fa06d';
+weatherRequest.open('Get', apiURL, true);
+weatherRequest.send();
+
+weatherRequest.onload = function(){
+let weatherData= JSON.parse(weatherRequest.responseText);
+document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+let imagesrc = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+document.getElementById('weatherimage').src = imagesrc;
+}
