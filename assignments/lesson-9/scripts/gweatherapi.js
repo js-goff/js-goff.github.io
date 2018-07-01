aba25701e44234307ef7f20f9d87a09ee1933ff7
js-goff <38774971+js-goff@users.noreply.github.com>
2018-06-30 23:35:53 -0600

    few dozen more changes.
    here are more changes.

--- assignments/lesson-9/scripts/gweatherapi.js
+++ assignments/lesson-9/scripts/gweatherapi.js
@@ -2,13 +2,15 @@
 
 
 let weatherRequest = new XMLHttpRequest();
-let apiURL='https://api.openweathermap.org/data/2.5/weather?&id=4787117&units=imperial&APPID=4c5ecbb4699bafc4a2d884937c4fa06d';
+let apiURL='https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=4c5ecbb4699bafc4a2d884937c4fa06d';
 weatherRequest.open('Get', apiURL, true);
 weatherRequest.send();
 
 weatherRequest.onload = function(){
 let weatherData= JSON.parse(weatherRequest.responseText);
 document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+document.getElementById('current-cond').innerHTML = weatherData.weather[0].description;
+document.getElementById('wind-speed').innerHTML = weatherData.wind.speed;
 let imagesrc = 'https://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
 document.getElementById('weatherimage').src = imagesrc;
 }
