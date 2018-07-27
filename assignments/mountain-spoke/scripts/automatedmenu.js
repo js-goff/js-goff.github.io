fbd758ca760943dbdec8e5daa0e4407796bd721e
js-goff <38774971+js-goff@users.noreply.github.com>
2018-07-26 18:13:22 -0600

    moutain spoke changes
    here are some final changes.

---
+++ assignments/mountain-spoke/scripts/automatedmenu.js
@@ -0,0 +1,9 @@
+$(function(){
+		// All jQuery code goes here
+      $('.navigation > li').hover(function(){
+          $(this).addClass('openSesame');                       
+    },  function(){
+          $(this).removeClass('openSesame');
+    })
+
+	})
